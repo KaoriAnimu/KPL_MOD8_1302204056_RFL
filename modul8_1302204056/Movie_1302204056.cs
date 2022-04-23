ff97488697b005de50295ba39809418186d3fa23
KaoriAnimu <mkaorirahadj89@gmail.com>
2022-04-23 17:25:54 +0700

    Program Code MOD 8 KPL

---
+++ modul8_1302204056/Movie_1302204056.cs
@@ -0,0 +1,11 @@
+﻿namespace modul8_1302204056
+{
+    public class Movie_1302204056
+    {
+        private string Title { get; set; }
+        private string Director { get; set; }
+        private List<string> Stars { get; set; }
+        private String Description { get; set; }
+
+    }
+}
